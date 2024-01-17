d5f01a44c466a326748c0f17feac86b43ec55b6d
Md Christien <mdchristien12@gmail.com>
2024-01-17 11:47:44 +0100

    Refactor Education section

--- my-app/app/components/Education.tsx
+++ my-app/app/components/Education.tsx
@@ -4,10 +4,11 @@ const Education = () => {
   return (
     <div className="text-[#ADB7BE] mb-7">
       <h2 className="text-3xl">2020 - 2023</h2>
-      <h3 className="text-lg">University Of Buea.</h3>
+      <h3 className="text-lg font-semibold">University Of Buea.</h3>
       <h3 className="text-lg">
-        <span>Bachelor of Science</span> in Computer Science.
+        <span>Bachelor of Science</span>Degree (B.sc)
       </h3>
+      <h3 className="text-lg font-semibold">Computer Science</h3>
     </div>
   );
 };
