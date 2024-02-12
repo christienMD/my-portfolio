3f297bfa5c0e86363271f20ac9523ef383a1b5f1
Md Christien <mdchristien12@gmail.com>
2024-02-12 16:33:30 +0100

    add navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-95 ">
+    <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-95">
       <div className="flex flex-wrap items-center justify-between mx-auto p-5">
         <Link
           href="/"
--- app/projectData.ts
+++ app/projectData.ts
@@ -12,37 +12,28 @@ export interface Project {
 export const projectData: Project[] = [
   {
     id: 1,
-    title: "mdchristien Cloud Hosting",
+    title: "Amazone Clone",
     description:
-      "This is a Bueatiful , Responsive and Blazingly Fast Website for a cloud hosting company- Cloud Hosting for Pros.",
-    imgUrl: "/images/online-hosting.png",
-    tag: ["All", "Web"],
-    gitUrl: "https://github.com/christienMD/mdchristien",
-    previewUrl: "https://mdchristien.netlify.app/",
-    stack: ["HTML5", "CSS3", "JavaScript"],
-  },
-  {
-    id: 2,
-    title: "Game-Hub",
-    description:
-      "This is a beautiful Video Game Discovery web app for searching and discovering games, It is a mini version of the Rawg website , popular Video Game Discovery Platform.",
-    imgUrl: "/images/gameHub-small.png",
-    gitUrl: "https://github.com/christienMD/game-hub",
-    previewUrl: "https://mdchristien-gamehub.vercel.app/",
+      "This is a Beautifull Full-stack Production-grade web app for tracking issues. This web app includes feature such as: Dashboard, Charts, Sorting, Filtering, Pagination, Skeletons , CRUD , Forms, Caching, Authentication and more.",
+    imgUrl: "/images/amazone-clone.png",
+    gitUrl: "https://github.com/christienMD/amazone-clone",
+    previewUrl: "https://issue-tracker-md-christien.vercel.app/",
     tag: ["All", "Web"],
     stack: [
       "HTML5",
       "CSS3",
       "JavaScript",
       "TypeScript",
-      "React Js",
-      "Zustand",
-      "Chakra UI",
-      "React Query",
+      "Next Js",
+      "React Context",
+      "Reducers",
+      "Axios",
+      "Tailwind CSS",
+      "NextAuth.js",
     ],
   },
   {
-    id: 3,
+    id: 2,
     title: "Issue Tracker",
     description:
       "This is a Beautifull Full-stack Production-grade web app for tracking issues. This web app includes feature such as: Dashboard, Charts, Sorting, Filtering, Pagination, Skeletons , CRUD , Forms, Caching, Authentication and more.",
@@ -63,6 +54,38 @@ export const projectData: Project[] = [
       "Prisma",
       "NextAuth.js",
     ],
+  },
+
+  {
+    id: 3,
+    title: "Game-Hub",
+    description:
+      "This is a beautiful Video Game Discovery web app for searching and discovering games, It is a mini version of the Rawg website , popular Video Game Discovery Platform.",
+    imgUrl: "/images/gameHub-small.png",
+    gitUrl: "https://github.com/christienMD/game-hub",
+    previewUrl: "https://mdchristien-gamehub.vercel.app/",
+    tag: ["All", "Web"],
+    stack: [
+      "HTML5",
+      "CSS3",
+      "JavaScript",
+      "TypeScript",
+      "React Js",
+      "Zustand",
+      "Chakra UI",
+      "React Query",
+    ],
+  },
+  {
+    id: 4,
+    title: "mdchristien Cloud Hosting",
+    description:
+      "This is a Bueatiful , Responsive and Blazingly Fast Website for a cloud hosting company- Cloud Hosting for Pros.",
+    imgUrl: "/images/online-hosting.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/christienMD/mdchristien",
+    previewUrl: "https://mdchristien.netlify.app/",
+    stack: ["HTML5", "CSS3", "JavaScript"],
   },
 ];
 
