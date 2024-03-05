b14711a6e4746672ec49235f5f453d418c8185cf
Md Christien <mdchristien12@gmail.com>
2024-03-05 21:20:11 +0100

    Refactor skills section

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -9,43 +9,43 @@ const Skills = () => {
         <Progress
           placeholder=""
           value={95}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>JavaScript + ES6</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>TypeScript</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>React Js</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Next Js</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Git/GitHub</h5>
         <Progress
           placeholder=""
           value={80}
-          color="indigo"
-          className="mb-2  bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1  bg-[#ADB7BE]"
         />
       </div>
       <div>
@@ -53,43 +53,43 @@ const Skills = () => {
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2  bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1  bg-[#ADB7BE]"
         />
         <h5>Zustand</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Tailwind CSS + Material Tailwind</h5>
         <Progress
           placeholder=""
           value={75}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Chakra UI</h5>
         <Progress
           placeholder=""
           value={90}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Radix UI</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
         <h5>Bootstrap</h5>
         <Progress
           placeholder=""
           value={70}
-          color="indigo"
-          className="mb-2 bg-[#ADB7BE]"
+          color="purple"
+          className="mb-4 mt-1 bg-[#ADB7BE]"
         />
       </div>
     </div>
